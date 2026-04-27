843ee031789aeb74c759dc4bb0e7d0a14fc97776
Lorvickd <grcialornzo@gmail.com>
2026-04-27 10:08:17 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+let currentSlide = 0;
+const slides = document.querySelectorAll(".slide");
 
+function nextSlide() {
+  slides[currentSlide].classList.remove("active");
+  currentSlide++;
+
+  if (currentSlide >= slides.length) {
+    currentSlide = 0;
+  }
+
+  slides[currentSlide].classList.add("active");
+}
